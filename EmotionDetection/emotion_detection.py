52ded22deab6a9c2d8b1b4e41db7fc0c959962f7
0xRig3l <paroxsymalrage@gmail.com>
2026-07-14 19:52:57 -0300

    add proper error handling

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -2,14 +2,27 @@ import requests
 
 def emotion_detector(text_to_analyze):
     """
-    This function analyzes the emotion of the text_to_analyze.
+    This function analyzes the emotion of the text_to_analyze using Watson AI API.
+    It returns a dictionary with the emotions and their respective confidence,
+    as well as a dominant emotion.
     """
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
-    input_json = { "raw_document": { "text": text_to_analyze } }
+    input_json = {"raw_document": {"text": text_to_analyze}}
 
-    res_dict = requests.post(url, headers=headers, json=input_json).json()
-    emotions_scores = res_dict["emotionPredictions"][0]["emotion"]
-    dominant_emotion = max(emotions_scores, key=emotions_scores.get)    
+    res = requests.post(url, headers=headers, json=input_json)
+
+    if res.status_code == 400:
+        return {
+            "anger": None,
+            "disgust": None,
+            "fear": None,
+            "joy": None,
+            "sadness": None,
+            "dominant_emotion": None,
+        }
+
+    emotions_scores = res.json()["emotionPredictions"][0]["emotion"]
+    dominant_emotion = max(emotions_scores, key=emotions_scores.get)
 
     return {**emotions_scores, "dominant_emotion": dominant_emotion}
--- server.py
+++ server.py
@@ -1,19 +1,35 @@
-from EmotionDetection import emotion_detector
+"""
+Server for the emotion detection flask application
+"""
 from flask import Flask, request, render_template
+from EmotionDetection import emotion_detector
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+    """
+    Index page
+    """
     return render_template('index.html')
+
 
 @app.route("/emotionDetector")
 def detector():
+    """
+    Function to perform the emotion detection on endpoint /emotionDetector.
+    It returns a formatted text with the output of the emotion detector function.
+    """
     text = request.args.get('textToAnalyze')
     emotions = emotion_detector(text)
     dominant_emotion = emotions["dominant_emotion"]
 
-    return f"For the given statement, the system response is 'anger': {emotions['anger']}, 'disgust': {emotions['disgust']}, 'fear': {emotions['fear']}, 'joy': {emotions['joy']} and 'sadness': {emotions['sadness']}. The dominant emotion is {dominant_emotion}."
+    if any(emotion is None for emotion in emotions.values()):
+        return "Invalid text! Please try again!"
+    return (f"For the given statement, the system response is 'anger': {emotions['anger']},"
+            f"'disgust': {emotions['disgust']}, 'fear': {emotions['fear']}, 'joy': "
+            f"{emotions['joy']} and 'sadness': {emotions['sadness']}."
+            f" The dominant emotion is {dominant_emotion}.")
 
 if __name__ == "__main__":
     app.run(debug=True)
